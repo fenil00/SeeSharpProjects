538e42b84396a9696b7aea0211d38b1cb87163d8
Fenil Desai <fenildesai00@gmail.com>
2023-01-04 11:47:07 +0100

    logger in dataAccess added

--- Source/DependencyInjectionLibrary/BetterBussinessLogic.cs
+++ Source/DependencyInjectionLibrary/BetterBussinessLogic.cs
@@ -8,14 +8,14 @@ namespace DependencyInjectionLibrary
 {
     using DependencyInjectionLibrary.Utilities;
 
-    public class BussinessLogic : IBussinessLogic
+    public class BetterBussinessLogic : IBussinessLogic
     {
         private ILogger _logger;
 
         private IDataAccess _dataAccess;
 
         //Constructor injection
-        public BussinessLogic(ILogger logger, IDataAccess dataAccess)
+        public BetterBussinessLogic(ILogger logger, IDataAccess dataAccess)
         {
             this._logger = logger;
             this._dataAccess = dataAccess;
@@ -23,14 +23,14 @@ namespace DependencyInjectionLibrary
 
         public void ProcessData()
         {
-            // don not need this anymore
-            // Logger logger = new Logger();
-            // DataAccess dataAccess = new DataAccess();
 
             _logger.Log("Starting the processing of data.");
+            Console.WriteLine();
             Console.WriteLine("Processing the data");
+            Console.WriteLine();
             _dataAccess.LoadData();
             _dataAccess.SaveData("Processed Info");
+            Console.WriteLine();
             _logger.Log("Finished processing of the data.");
 
         }
--- Source/DependencyInjectionLibrary/Utilities/DataAccess.cs
+++ Source/DependencyInjectionLibrary/Utilities/DataAccess.cs
@@ -6,16 +6,27 @@ using System.Threading.Tasks;
 
 namespace DependencyInjectionLibrary.Utilities
 {
+    using System.CodeDom;
+
     public class DataAccess : IDataAccess
     {
+        private ILogger _logger;
+
+        public DataAccess(ILogger logger)
+        {
+            this._logger = logger;
+        }
+
         public void LoadData()
         {
             Console.WriteLine("Loading Data");
+            this._logger.Log("Loading Data");
         }
 
         public void SaveData(string name)
         {
             Console.WriteLine($"Saving {name}");
+            this._logger.Log("Saving Data");
         }
     }
 }
