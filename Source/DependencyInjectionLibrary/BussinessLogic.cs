913c20436c443b335a3075c64e71d531031c5d44
Desai, Fenilkumar <fenilkumar.desai@schulergroup.com>
2023-01-04 08:08:59 +0100

    DependencyInjection projects added

---
+++ Source/DependencyInjectionLibrary/BussinessLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionLibrary
+{
+    public class BussinessLogic
+    {
+        public void ProcessData()
+        {
+
+        }
+    }
+}
