43b58b0c88eb9b6c327451932aa717eb9d322073
Fenil Desai <fenildesai00@gmail.com>
2023-01-04 15:19:10 +0100

    Func and Action added

--- Source/DelegatesConsoleUI/Program.cs
+++ Source/DelegatesConsoleUI/Program.cs
@@ -20,7 +20,25 @@ namespace DelegatesConsoleUI
 
             PopulateCartWithDemoData();
 
-             Console.WriteLine($"The total for the cart is {cart.GenerateTotal(SubTotalAlert):C2}");
+             Console.WriteLine($"The total for the cart is {cart.GenerateTotal(SubTotalAlert, CalulateLeveledDiscount, AlertUser):C2}");
+
+             Console.WriteLine();
+
+            decimal total = cart.GenerateTotal((subTotal) => Console.WriteLine($"Subtotal for cart 2 is {subTotal:C2}"),
+                 (prodcuts, subtotal) =>
+                     {
+                         if (prodcuts.Count > 3)
+                         {
+                             return subtotal * 0.5M;
+                         }
+                         else
+                         {
+                             return subtotal;
+                         }
+                     },
+                 (act) => Console.WriteLine($"Cart 2 Alert: {act}"));
+
+             Console.WriteLine($"The total for car 2 is {total:C2}");
 
              Console.WriteLine();
              Console.Write("Please press any key to exit the application...");
@@ -30,6 +48,31 @@ namespace DelegatesConsoleUI
         private static void SubTotalAlert(decimal subTotal)
         {
             Console.WriteLine($"The subtotal is {subTotal:C2}");
+        }
+
+        private static void AlertUser(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private static decimal CalulateLeveledDiscount(List<ProductModel> items, decimal subTotal)
+        {
+            if (subTotal > 100)
+            {
+                return subTotal * 0.80M;
+            }
+            else if (subTotal > 50)
+            {
+                return subTotal * 0.85M;
+            }
+            else if (subTotal > 10)
+            {
+                return subTotal * 0.95M;
+            }
+            else
+            {
+                return subTotal;
+            }
         }
 
         private static void PopulateCartWithDemoData()
--- Source/DelegatesLibrary/ShoppingCartModel.cs
+++ Source/DelegatesLibrary/ShoppingCartModel.cs
@@ -13,28 +13,18 @@ namespace DelegatesLibrary
         public List<ProductModel> Items { get; set; } = new List<ProductModel>();
 
 
-        public decimal GenerateTotal(MentionDiscount mentionDiscount)
+        public decimal GenerateTotal(MentionDiscount mentionSubtotal,
+                                     Func<List<ProductModel>,decimal,decimal> calulateDiscountedTotal,
+                                     Action<string> tellUserWeAreDiscounting)
         {
             decimal subTotal = Items.Sum(x => x.Price);
 
-            mentionDiscount(subTotal);
+            mentionSubtotal(subTotal);
 
-            if (subTotal > 100)
-            {
-                return subTotal * 0.80M;
-            }
-            else if(subTotal > 50)
-            {
-                return subTotal * 0.85M;
-            }
-            else if (subTotal > 10)
-            {
-                return subTotal * 0.90M;
-            }
-            else
-            {
-                return subTotal;
-            }
+            tellUserWeAreDiscounting("We are applying your discount.");
+
+            return calulateDiscountedTotal(Items, subTotal);
+            
         }
     }
 }
