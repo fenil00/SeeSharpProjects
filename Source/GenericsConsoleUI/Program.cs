95daf0e1909741fa6aec22f36dd79c6c7b09f785
Fenil Desai <fenildesai00@gmail.com>
2023-01-07 11:06:53 +0100

    models added

--- Source/GenericsConsoleUI/Program.cs
+++ Source/GenericsConsoleUI/Program.cs
@@ -6,10 +6,47 @@ using System.Threading.Tasks;
 
 namespace GenericsConsoleUI
 {
+    using GenericsConsoleUI.Models;
+    using GenericsConsoleUI.WithoutGenerics;
+
     class Program
     {
         static void Main(string[] args)
         {
+            DemonstrateTextFileStorage();
+            Console.WriteLine();
+            Console.Write("Press enter to Shut down...");
+            Console.ReadLine();
+        }
+
+        private static void DemonstrateTextFileStorage()
+        {
+            List<Person> people = new List<Person>();
+            List<LogEntry> logs = new List<LogEntry>();
+            string peopleFile = @"D:\C#Practises\Temp\people.csv";
+            string logFile = @"D:\C#Practises\Temp\logs.csv";
+
+            PopulateLists(people, logs);
+
+            OriginalTextFileProcessor.SavePeople(people, peopleFile);
+
+            var newPeople = OriginalTextFileProcessor.LoadPeople(peopleFile);
+
+            foreach (var p in newPeople)
+            {
+                Console.WriteLine($"{ p.FirstName } { p.LastName } (IsAlive = { p.IsAlive })");
+            }
+        }
+
+        private static void PopulateLists(List<Person> people, List<LogEntry> logs)
+        {
+            people.Add(new Person { FirstName = "Tim", LastName = "Corey" });
+            people.Add(new Person { FirstName = "Sue", LastName = "Storm", IsAlive = false });
+            people.Add(new Person { FirstName = "Greg", LastName = "Olsen" });
+
+            logs.Add(new LogEntry { Message = "I blew up", ErrorCode = 9999 });
+            logs.Add(new LogEntry { Message = "I'm too awesome", ErrorCode = 1337 });
+            logs.Add(new LogEntry { Message = "I was tired", ErrorCode = 2222 });
         }
     }
 }
