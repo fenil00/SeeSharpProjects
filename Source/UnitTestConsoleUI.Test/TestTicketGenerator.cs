af330c9201db22a11b019d5208c12d04616d0e20
Fenil Desai <fenildesai00@gmail.com>
2023-01-17 13:01:42 +0100

    Unit test callback added

---
+++ Source/UnitTestConsoleUI.Test/TestTicketGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestConsoleUI.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Moq;
+
+    [TestClass]
+    public class TestTicketGenerator
+    {
+        [TestMethod]
+        public void TestTicket()
+        {
+            Person person = new Person { FirstName = "Bob", LastName = "Smith", Age = 18 };
+
+            Mock<IPersonNameCleaner> _mockCleaner = new Mock<IPersonNameCleaner>();
+
+            var ticketGenerator = new TicketGenerator(_mockCleaner.Object);
+
+            var ticket = ticketGenerator.GenerateTicket(person);
+
+            Assert.AreEqual("B Smith", ticket.Name);
+        }
+
+        [TestMethod]
+        public void TestTicketWithCallback()
+        {
+            Person person = new Person { FirstName = "Bob", LastName = "Smith", Age = 18 };
+
+            Mock<IPersonNameCleaner> _mockCleaner = new Mock<IPersonNameCleaner>();
+            _mockCleaner.Setup(m => m.Clean(It.IsAny<Person>()))
+                .Callback(() => person.FirstName = "B");
+
+            var ticketGenerator = new TicketGenerator(_mockCleaner.Object);
+
+            var ticket = ticketGenerator.GenerateTicket(person);
+
+            Assert.AreEqual("B Smith", ticket.Name);
+        }
+
+        [TestMethod]
+        public void TestTicketWithCallbackParameter()
+        {
+            Person person = new Person { FirstName = "Bob", LastName = "Smith", Age = 18 };
+
+            Mock<IPersonNameCleaner> _mockCleaner = new Mock<IPersonNameCleaner>();
+            _mockCleaner.Setup(m => m.Clean(It.IsAny<Person>()))
+                .Callback<Person>(p => p.FirstName = p.FirstName[0].ToString());
+
+            var ticketGenerator = new TicketGenerator(_mockCleaner.Object);
+
+            var ticket = ticketGenerator.GenerateTicket(person);
+
+            Assert.AreEqual("B Smith", ticket.Name);
+        }
+    }
+}
---
+++ Source/UnitTestConsoleUI/TicketGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestConsoleUI
+{
+    public class TicketGenerator
+    {
+        IPersonNameCleaner _cleaner;
+
+        public TicketGenerator(IPersonNameCleaner cleaner)
+        {
+            _cleaner = cleaner;
+        }
+
+        public Ticket GenerateTicket(Person person)
+        {
+            _cleaner.Clean(person);
+            return new Ticket
+                       {
+                           Name = string.Format("{0} {1}", person.FirstName, person.LastName)
+                       };
+        }
+    }
+}
